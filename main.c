0442432af24601877dac9aa4684ff6630ff26872
idelvina <73911781+idelvina@users.noreply.github.com>
2020-11-04 00:31:16 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/*autor:Idel
+email: idelvinafernando@gmail.com
+data:27/10/2020*/
+
+
+
+
+
+#include <stdio.h>
+ int main() 
+ {
+     /* como funciona*/
+
+      int x = 1;
+       printf("%d\n", x);
+      
+       }
+
+
+ 
