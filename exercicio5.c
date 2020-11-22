ff68beb1d4f6165e4b7afd0666e68b37a2f0423f
idelvina <73911781+idelvina@users.noreply.github.com>
2020-11-22 21:28:19 +0000

    Add files via upload

--- exercicio5.c
+++ exercicio5.c
@@ -1,20 +1,37 @@
+/**
+ * @file exercicio5.c
+ * @author Idelvina Fernando e Joel Jonassi
+ * @brief Somatorio
+ * @version 0.1
+ * @date 2020-11-22
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #include <stdio.h>
-long somatorio(int n); //funcao somatorio, n para entrada de dados 
+int somatorio(int n); //funcao somatorio, n para entrada de dados 
 
-int main(void)
+int main()
 {
-    int num;
-    printf("insira um numero natural\n"); //escrever numero
-    scanf("%d", &num); //ler o numero
-    printf(" O somatorio de %d é igual a %d", num, somatorio(num));
+    int n;
+    printf("Insira um numero natural: \n");
+    scanf("%d", &n); 
+    printf(" O somatorio de 1 a %d = %d", n, somatorio(n));
 }
-
-long somatorio(int n)//funcao somatorio com parametro n
-{
-    int i, x;// duas variaveis do tipo inteiro
-    for(i = 1, x = 0; i <= n; i ++)//
-    {
-        x = x + i;//x recebe x mais i, e faz o somatorio
-    }
-    return(x);
-}
+/**
+ * @brief função que calcula o somátorio
+ * @param n varivel de entrada de dados para o somatorio de n elementos
+ * @param i valor inicial
+ * @param x x recebe x + i, e faz o somatorio
+ * @return int 
+ * 
+ */
+    int somatorio(int n)
+        {
+            int i, x;
+            for(i = 1, x = 0; i <= n; i ++)
+                {
+                    x = x + i;
+                }
+            return(x);
+        }
